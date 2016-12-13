2da581f5603fc494725914aad8a9fca2b6bab4aa
Nada Fadali <nada.fadali@gmail.com>
2016-12-13 16:39:37 +0200

    yarab

--- src/app/hasalat/show/show.js
+++ src/app/hasalat/show/show.js
@@ -1,7 +1,8 @@
 angular
   .module('hasalaNgo')
-  .controller('hasalatShowController', ['$stateParams', 'getHasalaDetails', function ($stateParams, getHasalaDetails) {
+  .controller('hasalatShowController', ['$stateParams', 'getHasalaDetails', '$uibModal', function ($stateParams, getHasalaDetails, $uibModal) {
     // this.show = getHasalaDetails.getHasala($stateParams.hasalaId);
+    var vm = this;
     this.details = getHasalaDetails;
 
     // handle dates
@@ -10,8 +11,40 @@ angular
       return tmp.toLocaleString();
     };
     // mark milestones
-    this.completeMilestone = function (index) {
+    vm.completeMilestone = function (index) {
       this.details.milestones[index].done = 1;
+    };
+    // milestone modal
+    this.openMilestoneModal = function (size, parentSelector, index) {
+      var parentElem = parentSelector ? angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+      var modalInstance = $uibModal.open({
+        animation: true,
+        ariaLabelledBy: 'modal-title',
+        ariaDescribedBy: 'modal-body',
+        templateUrl: 'modalContent.html',
+        controller: 'ModalInstanceController',
+        controllerAs: '$ctrl',
+        size: size,
+        appendTo: parentElem
+      });
+      modalInstance.result.then(function (status) {
+        if (status) {
+          vm.completeMilestone(index);
+        }
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+  }])
+  .controller('ModalInstanceController', ['$uibModalInstance', function ($uibModalInstance) {
+    this.status = 0;
+
+    this.ok = function () {
+      this.status = 1;
+      $uibModalInstance.close(this.status);
+    };
+    this.cancel = function () {
+      $uibModalInstance.dismiss('cancel');
     };
   }])
   .factory('getHasalaDetails', [function () {
@@ -33,6 +66,7 @@ var data = {
   donation: 25,
   target: 1000,
   status: 'expired',
+  isEditable: 1,
 
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 

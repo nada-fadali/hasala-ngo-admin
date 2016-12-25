7ab091925dbb1e8f1dd2f4c37d8509344e8357de
Nada Fadali <nada.fadali@gmail.com>
2016-12-25 18:40:02 +0200

    yarab

--- src/app/ngo/edit/edit.js
+++ src/app/ngo/edit/edit.js
@@ -1,7 +1,16 @@
 angular
   .module('hasalaNgo')
   .controller('ngoEditController', ['$stateParams', 'sendEditNgo', function ($stateParams, sendEditNgo) {
-    this.ngoData = $stateParams.ngo;
+    var vm = this;
+    vm.ngoData = $stateParams.ngo;
+
+    this.deleteCategory = function (index) {
+      vm.ngoData.categories.splice(index, 1);
+    };
+
+    this.addCategory = function () {
+      vm.ngoData.categories.push('');
+    };
   }])
   .factory('sendEditNgo', [function () {
     return this;

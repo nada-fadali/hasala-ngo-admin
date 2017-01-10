4996c39f3ea6092cb54a9c2993898f3a2ce087e2
Nada Fadali <nada.fadali@gmail.com>
2017-01-10 20:54:59 +0200

    yarab

--- src/app/hasalat/index/index.js
+++ src/app/hasalat/index/index.js
@@ -1,9 +1,15 @@
 angular
   .module('hasalaNgo')
   .controller('hasalatIndexController', ['getHasalatList', function (getHasalatList) {
+    var vm = this;
     this.ngo = "Masr El Kheir";
     this.list = getHasalatList;
     this.categories = categories;
+    this.selectedCategory = null;
+
+    this.filterHasalat = function (category) {
+      vm.selectedCategory = category;
+    };
   }])
   .factory('getHasalatList', [function () {
     return hasalatList.reverse();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 angular
-  .module('hasalaNgo', ['ui.bootstrap', 'picardy.fontawesome', 'ngQuill', 'ui.router'])
+  .module('hasalaNgo', ['ui.bootstrap', 'picardy.fontawesome', 'ngQuill', 'angular.filter', 'ui.router'])
   .config(['ngQuillConfigProvider', function (ngQuillConfigProvider) {
     ngQuillConfigProvider.set();
   }]);
